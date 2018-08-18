c47e246325f5c185ce21b8fba518ea1e32c2ca31
MOHAMEDKASIM <MOHAMEDKASIM@KASIM>
2018-08-18 22:45:42 +0530

    commit new code for factorial

---
+++ Selenium1/src/main/java/factorial.java
@@ -0,0 +1,16 @@
+
+public class factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int number = 4;
+int factorial=1;
+int i;
+for(i=1;i<=number;i++)
+{
+	factorial=factorial*i;
+}
+System.out.println("The factorial of "+number+" is " +factorial);
+	}
+
+}
